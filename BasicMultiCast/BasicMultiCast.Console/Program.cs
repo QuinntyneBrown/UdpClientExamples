5336d3e35e9bb7f9962acdd25eed2e99c16ad82e
QuinntyneBrown <quinntynebrown@gmail.com>
2023-02-04 10:55:14 -0500

    update

--- BasicMultiCast/BasicMultiCast.Console/Program.cs
+++ BasicMultiCast/BasicMultiCast.Console/Program.cs
@@ -3,11 +3,29 @@
 
 var tcs = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
 
+var udpClient1 = new UdpClient();
+
+string multiCastGroupIp = "224.0.0.1";
+
+udpClient1.Client.Bind(IPEndPoint.Parse("127.0.0.1:80"));
+
+udpClient1.JoinMulticastGroup(IPAddress.Parse(multiCastGroupIp), IPAddress.Parse("127.0.0.1"));
+
+var udpClient2 = new UdpClient();
+
+udpClient2.Client.Bind(IPEndPoint.Parse("127.0.0.2:80"));
+
+udpClient2.JoinMulticastGroup(IPAddress.Parse(multiCastGroupIp), IPAddress.Parse("127.0.0.2"));
+
 _ = Task.Run(async () =>
 {
+    var result = await udpClient1.ReceiveAsync();
 
     tcs.SetResult();
 });
 
+var bytesToSend = new byte[0];
+
+await udpClient2.SendAsync(bytesToSend, multiCastGroupIp, 80);
 
 await tcs.Task;
