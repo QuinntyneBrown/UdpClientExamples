f8aeca5c593ccce59e4c6171a4417a74768aa540
QuinntyneBrown <quinntynebrown@gmail.com>
2023-02-04 10:33:46 -0500

    intial

---
+++ BasicPointToPoint/BasicPointToPoint.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+var tcs = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+
+var udpClient1 = new UdpClient();
+
+udpClient1.Client.Bind(IPEndPoint.Parse("127.0.0.1:80"));
+
+var udpClient2 = new UdpClient();
+
+udpClient2.Client.Bind(IPEndPoint.Parse("127.0.0.2:80"));
+
+_ = Task.Run(async () =>
+{
+    var result = await udpClient1.ReceiveAsync();
+
+    tcs.SetResult();
+});
+
+var bytesToSend = new byte[0];
+
+await udpClient2.SendAsync(bytesToSend, IPEndPoint.Parse("127.0.0.1:80"));
+
+await tcs.Task;
